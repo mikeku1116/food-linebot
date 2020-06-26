814abcdb151ad4c5573e29c12fbce75fac622f1c
mikeku1116 <mikekuforgit@gmail.com>
2020-06-26 22:38:31 +0800

    建立foodlinebot應用程式

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'foodlinebot.apps.FoodlinebotConfig',
 ]
 
 MIDDLEWARE = [
